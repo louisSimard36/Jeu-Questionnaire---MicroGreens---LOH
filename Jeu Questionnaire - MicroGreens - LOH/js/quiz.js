39969e6c873a7b054f870ccf1cf064810f4014d3
louisSimard36 <45235565+louisSimard36@users.noreply.github.com>
2018-12-05 09:20:55 -0400

    Update quiz.js

--- Jeu Questionnaire - MicroGreens - LOH/js/quiz.js
+++ Jeu Questionnaire - MicroGreens - LOH/js/quiz.js
@@ -5,6 +5,9 @@
  * @copyright 2018
  */
 
+var bonneRep;
+var bonneRepTexte;
+
 function determinerChoixUtilisateur1()
 {
 	reponseUtilisateur = 1;
@@ -22,29 +25,36 @@ function determinerChoixUtilisateur4()
 	reponseUtilisateur = 4;
 }
 
-function validerQuestion (determinerChoixUtilisateur1, determinerChoixUtilisateur2, determinerChoixUtilisateur3, determinerChoixUtilisateur4)
+function validerQuestion(determinerChoixUtilisateur1, determinerChoixUtilisateur2, determinerChoixUtilisateur3, determinerChoixUtilisateur4)
 {
-	if (reponseUtilisateur !== questionsQuiz[questionCourante - 1][1])
+	if(reponseUtilisateur !== questionsQuiz[questionCourante - 1][1])
 	{
-		window.prompt("Mauvaise réponse", questionsQuiz[questionCourante - 1][2]);
+		bonneRep = questionsQuiz[questionCourante - 1][1];
+		bonneRepTexte = bonneRep + 2;
+		document.getElementById("texteReponse").textContent = questionsQuiz[questionCourante - 1][bonneRepTexte];
+		document.getElementById("lienPlusInfos").textContent = questionsQuiz[questionCourante - 1][2];
+		document.getElementById("lienPlusInfos").href = questionsQuiz[questionCourante - 1][2];
+		$("#modalReponse").modal();
+		jouerSon("errorAudio");
 	}
 	else
 	{
-		window.prompt("Bonne Réponse", questionsQuiz[questionCourante - 1][2])
+		ajouterPoint();
 	}
+
 }
+
+
 
 /**
  * @name ajouterPoint
  * @description Ajoute un point au total des points.
  */
-
-function ajouterPoint()
+function ajouterPoint(estFinPartie)
 {
-	if (reponseUtilisateur == questionsQuiz[questionCourante - 1][1] && totalPointage <= MAX_QUESTIONS)
-	{
 		document.getElementById("totalPoints").textContent = totalPointage ++;
-	}
+		jouerSon("successAudio");
+	
 }
 
 /**
@@ -52,7 +62,6 @@ function ajouterPoint()
  * @description Obtiens le pointage total accumulé.
  * @returns Le pointage total
  */
-
 function obtenirPointage()
 {
 
@@ -64,21 +73,12 @@ function obtenirPointage()
  * @param {*} questionCourante Index de la question courante
  * @returns true si l'index de la question courrante est égal au nombre maximum de questions, sinon faux
  */
-
-function estFinPartie(questionCourante)
+function estFinPartie()
 {
-	console.log(finPartie)
-	var finPartie = true;
-
-	if (questionCourante < (MAX_QUESTIONS - 1))
+	if (questionCourante == MAX_QUESTIONS)
 	{
-		finPartie = false;
+		afficherBoiteFinDeJeu();
 	}
-	else
-	{
-		finPartie = true;
-	}
-	return finPartie;	
 }
 
 /**
@@ -87,28 +87,27 @@ function estFinPartie(questionCourante)
  */
 
 function chargerQuestion()
-{
-	document.getElementById("texteQuestion").textContent = questionsQuiz[0][0];
-	document.getElementById("boitesChoix").style.display = "block";
-}
+	{
+		document.getElementById("texteQuestion").textContent = questionsQuiz[0][0];
+		document.getElementById("boitesChoix").style.display = "block";
+	}
 
 function questionSuivante()
 {
-	if (questionCourante < (MAX_QUESTIONS - 1))
+	if (questionCourante < MAX_QUESTIONS)
 	{
+		estFinPartie();
 		questionCourante ++;
 		document.getElementById("texteQuestion").textContent = questionsQuiz[questionCourante][0];
 		document.getElementById("noQuestionCourante").textContent = (questionCourante + 1);
 	}
 }
-
 /**
  * @name obtenirBonneReponse
  * @description Incrémente l'index indiquant la question courante.
  * @param {*} noQuestion L'index de la question
  * @returns retourne la bonne réponse
  */
-
 function obtenirBonneReponse(noQuestion)
 {
 	//ajouter votre code ici
@@ -130,12 +129,12 @@ function majTexteChoix()
 	document.getElementById("btnChoix4").textContent = questionsQuiz[questionCourante][6];
 }
 
+
 /**
  * @name majTexteQuestion
  * @description Modifie l'interface en affichant une question.
  * @param {*} noQuestion Index de la question qu'il faut afficher.
  */
-
 function majTexteQuestion(noQuestion)
 {
 	//ajouter votre code ici
@@ -145,11 +144,12 @@ function majTexteQuestion(noQuestion)
 	$('#texteQuestion').addClass('animated bounceInLeft delay-1s');
 }
 
+
+
 /**
  * @name remiseAZeroBoutons
  * @description Modifie l'interface en remettant à l'état initial les boutons de réponse.
  */
-
 function remiseAZeroBoutons()
 {
 	//ajouter votre code ici
@@ -159,7 +159,6 @@ function remiseAZeroBoutons()
  * @name majProgression
  * @description Modifie l'interface en ajustant la barre de progression.
  */
-
 function majProgression()
 {
 	//ajouter votre code ici
@@ -169,7 +168,6 @@ function majProgression()
  * @name majInterface
  * @description Modifie l'interface en changeant la question, les choix de réponses, en mettant à jour le pointage, la barre de progression et le numéro de la question courante et en remettant à zéro les boutons.
  */
-
 function majInterface()
 {
 	//ajouter votre code ici
@@ -180,7 +178,6 @@ function majInterface()
  * @description Modifie l'interface pour changer l'apparence du bouton cliqué et activer le bouton Valider.
  * @param {*} noChoix Numéro du choix de réponse sélectionné.
  */
-
 function selectionnerChoix(noChoix)
 {
 	//ajouter votre code ici
@@ -190,17 +187,11 @@ function selectionnerChoix(noChoix)
  * @name afficherBoiteFinDeJeu
  * @description Modifie l'interface pour afficher la boîte de résumé et cacher la boîte de question.
  */
-
-function afficherBoiteFinDeJeu(estFinPartie)
+function afficherBoiteFinDeJeu()
 {
-	if (estFinPartie == true)
-	{
-		var recommencer = confirm("La partie est terminée, voulez-vous réessayer?");
-
-		if (recommnecer == true)
-		{
-			location.reload();
-			return true;
-		}
-	}
+	document.getElementById("texteQuestion").textContent = "Félicitations, la partie est terminée! Votre score final est de: ";
+	document.getElementById("scoreFinal").textContent = totalPointage - 1;
+	document.getElementById("boitesChoix").style.display = "none";
+	document.getElementById("affichagePoints").style.display = "none";
+	document.getElementById("affichageQuestion").style.display = "none";
 }
